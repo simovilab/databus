2bb3509c17d4e0d01d4aaa61f058dcb1dd263dc4
Fabián Abarca <ensinergia@gmail.com>
2024-01-19 11:27:43 -0300

    Create synthetic_data.py

---
+++ synthetic_data.py
@@ -0,0 +1,2 @@
+# Crear posición, velocidad, dirección, ocupación, odómetro.
+# Requiere de acceso a tablas de GTFS Schedule. Esto puede ser con archivos CSV, por ejemplo, o con una base de datos, como SQLite (un archivo binario)
