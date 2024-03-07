8fa02c6c5202b8cbc411e4159b7c3817cfa83dcc
Fabián Abarca <ensinergia@gmail.com>
2024-03-07 15:21:39 -0300

    Primera versión simplificada de un FeedMessage de VehiclePosition \m/

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
 
 # Create your models here.
+
+
+class BusData(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    vehicle_id = models.CharField(max_length=100, blank=True, null=True)
+    route_id = models.CharField(max_length=100, blank=True, null=True)
+    trip_id = models.CharField(max_length=100, blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    start_time = models.TimeField(blank=True, null=True)
+    location_latitude = models.FloatField(blank=True, null=True)
+    location_longitude = models.FloatField(blank=True, null=True)
+    location_point = models.PointField(blank=True, null=True)
+    inertial_bearing = models.IntegerField(blank=True, null=True)
+    intertial_speed = models.FloatField(blank=True, null=True)
+    vehicle_health_fuel_level = models.FloatField(blank=True, null=True)
+    vehicle_health_oil_level = models.FloatField(blank=True, null=True)
+    environmental_temperature = models.FloatField(blank=True, null=True)
+
+    received_at = models.DateTimeField(auto_now_add=True)
+    is_processed = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        self.location_point = Point(self.location_longitude, self.location_latitude)
+        super(BusData, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.vehicle_id
--- feed/tasks.py
+++ feed/tasks.py
@@ -1,10 +1,41 @@
 from celery import shared_task
 
+from .models import BusData
+
+import json
+import datetime
+
 
 @shared_task
 def build_vehicle_position():
     print("Building feed VehiclePosition...")
-    # Query the database for new vehicle positions
+
+    unprocessed_data = BusData.objects.filter(is_processed=False)
+
+    feed_message = {}
+    feed_message["header"] = {}
+    feed_message["header"]["gtfs_realtime_version"] = "2.0"
+    feed_message["header"]["incrementality"] = "FULL_DATASET"
+    feed_message["header"]["timestamp"] = int(datetime.datetime.now().timestamp())
+    feed_message["entity"] = []
+
+    for vehicle in unprocessed_data:
+        vehicle_entity = {}
+        vehicle_entity["id"] = vehicle.vehicle_id
+        vehicle_entity["vehicle"] = {}
+
+        vehicle_entity["vehicle"]["vehicle"] = {}
+        vehicle_entity["vehicle"]["vehicle"]["id"] = vehicle.vehicle_id
+
+        feed_message["entity"].append(vehicle_entity)
+
+        vehicle.is_processed = True
+        vehicle.save()
+
+    feed_message_json = json.dumps(feed_message)
+    print(feed_message_json)
+
+    # Query the database ("BusData" table) for new vehicle positions
     # Create a new feed in a JSON format
     # Expose the JSON feed to the API (bus.ucr.ac.cr/gtfs/realtime/vehicle_position.json)
     # Save the feed to a binary protobuf file, available at a given URL (bus.ucr.ac.cr/gtfs/realtime/vehicle_position.pb)
@@ -19,5 +50,5 @@ def build_trip_update():
 
 @shared_task
 def build_alert():
-    print("Building feed Alert")
+    print("Building feed Alert...")
     return "Feed ServiceAlert built"
