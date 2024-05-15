b58131dd51a00930a6f1a414afcf1a98dfc867bf
Fabián Abarca <ensinergia@gmail.com>
2024-05-14 23:03:39 -0300

    Revisión integral del API, versión casi final

--- realtime/settings.py
+++ realtime/settings.py
@@ -136,7 +136,7 @@ REST_FRAMEWORK = {
 }
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "bUCR - Servidor en tiempo real",
+    "TITLE": "Databús API",
 }
 
 # Internationalization
@@ -155,7 +155,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
