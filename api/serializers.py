b8510c39452969704fdfae35cbf4edf95475ce61
Fabián Abarca <ensinergia@gmail.com>
2025-01-20 17:52:50 -0600

    Actualización de API para incluir GTFS Schedule

--- api/serializers.py
+++ api/serializers.py
@@ -8,10 +8,16 @@ from feed.models import (
     Progression,
     Occupancy,
 )
-from gtfs.models import Agency
+from gtfs.models import *
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from django.contrib.gis.geos import Point
+from rest_framework_gis.serializers import GeoFeatureModelSerializer, GeometryField
+
+
+# --------------
+# Telemetry data
+# --------------
 
 
 class VehicleSerializer(serializers.HyperlinkedModelSerializer):
@@ -155,3 +161,134 @@ class OccupancySerializer(serializers.HyperlinkedModelSerializer):
 class FindTripsSerializer(serializers.Serializer):
     trip_id = serializers.CharField()
     trip_departure_time = serializers.TimeField()
+
+
+# -------------
+# GTFS Schedule
+# -------------
+
+
+class AgencySerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Agency
+        fields = "__all__"
+
+
+class StopSerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Stop
+        fields = "__all__"
+
+
+class GeoStopSerializer(GeoFeatureModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+    stop_point = GeometryField()
+
+    class Meta:
+        model = Stop
+        geo_field = "stop_point"
+        fields = "__all__"
+
+
+class RouteSerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Route
+        fields = "__all__"
+
+
+class CalendarSerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Calendar
+        fields = "__all__"
+
+
+class CalendarDateSerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = CalendarDate
+        fields = "__all__"
+
+
+class ShapeSerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Shape
+        fields = "__all__"
+
+
+class GeoShapeSerializer(GeoFeatureModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+    geometry = GeometryField()
+
+    class Meta:
+        model = GeoShape
+        geo_field = "geometry"
+        fields = "__all__"
+
+
+class TripSerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Trip
+        fields = "__all__"
+
+
+class StopTimeSerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = StopTime
+        fields = "__all__"
+
+
+class FareAttributeSerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = FareAttribute
+        fields = "__all__"
+
+
+class FareRuleSerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = FareRule
+        fields = "__all__"
+
+
+class FeedInfoSerializer(serializers.HyperlinkedModelSerializer):
+
+    feed = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = FeedInfo
+        fields = "__all__"
+
+
+# -------------
+# GTFS Realtime
+# -------------
--- api/urls.py
+++ api/urls.py
@@ -14,6 +14,21 @@ router.register(r"journey", views.JourneyViewSet)
 router.register(r"position", views.PositionViewSet)
 router.register(r"progression", views.ProgressionViewSet)
 router.register(r"occupancy", views.OccupancyViewSet)
+# GTFS Schedule
+router.register(r"gtfs/schedule/agencies", views.AgencyViewSet)
+router.register(r"gtfs/schedule/stops", views.StopViewSet)
+router.register(r"gtfs/schedule/geo-stops", views.GeoStopViewSet, basename='geo-stop')
+router.register(r"gtfs/schedule/shapes", views.ShapeViewSet)
+router.register(r"gtfs/schedule/geo-shapes", views.GeoShapeViewSet)
+router.register(r"gtfs/schedule/routes", views.RouteViewSet)
+router.register(r"gtfs/schedule/calendars", views.CalendarViewSet)
+router.register(r"gtfs/schedule/calendar-dates", views.CalendarDateViewSet)
+router.register(r"gtfs/schedule/trips", views.TripViewSet)
+router.register(r"gtfs/schedule/stop-times", views.StopTimeViewSet)
+router.register(r"gtfs/schedule/fare-attributes", views.FareAttributeViewSet)
+router.register(r"gtfs/schedule/fare-rules", views.FareRuleViewSet)
+router.register(r"gtfs/schedule/feed-info", views.FeedInfoViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
