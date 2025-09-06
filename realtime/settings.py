20e464176bd3527244158e5cf23f225f88f09b9b
Alejandro Jimenez <alejimenezrj@gmail.com>
2025-09-06 13:03:32 -0600

    refactor: remove defaults from settings.py
    Co-authored-by: camsko <camfo@hotmail.com>
    Co-authored-by: Felipequesada21 <felipe.quesadaparada@ucr.ac.cr>
    Co-authored-by: disotocastro <juandisoca@gmail.com>

--- realtime/settings.py
+++ realtime/settings.py
@@ -95,9 +95,9 @@ DATABASES = {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
         "NAME": config("DB_NAME"),
         "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD", default=""),
+        "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT", default="5432"),
+        "PORT": config("DB_PORT"),
     },
 }
 
