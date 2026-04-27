4beb63f0aead023834a4bd37bbc6d827c8e0d002
Fabián Abarca Calderón <ensinergia@gmail.com>
2026-04-27 14:18:50 -0300

    chore: move the delivery of the GTFS files to the feed app

--- backend/feed/urls.py
+++ backend/feed/urls.py
@@ -1,1 +1,14 @@
-# Nothing here
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.status, name="status"),
+    path("schedule/feed.zip", views.schedule, name="schedule"),
+    path("realtime/vehicle_positions.json", views.vehicle_json, name="vehicle_json"),
+    path("realtime/vehicle_positions.pb", views.vehicle_pb, name="vehicle_pb"),
+    path(
+        "realtime/trip_updates.json", views.trip_updates_json, name="trip_updates_json"
+    ),
+    path("realtime/trip_updates.pb", views.trip_updates_pb, name="trip_updates_pb"),
+]
